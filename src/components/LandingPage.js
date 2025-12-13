f74cb5ae7f0e2a43525f0108fa2d7cd77be7daca
EMMANUELLA MALEKA <emmanuellamaleka@MacBook-Air-de-EMMANUELLA.local>
2025-12-13 14:45:12 +0100

    Adjust logo scroll speed to 25s (standard)

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -391,7 +391,7 @@ const LandingPage = () => {
                     .animate-scroll {
                         display: flex;
                         width: max-content;
-                        animation: scroll 40s linear infinite;
+                        animation: scroll 25s linear infinite;
                     }
                     .animate-scroll:hover {
                         animation-play-state: paused;
